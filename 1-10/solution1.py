03e61dee707c9a3b66b7c745d042754052d628c3
Jose Mugaburu <mugab686@gmail.com>
2017-08-22 00:56:12 -0400

    adds verified solution for problem 1 in python

--- 1-10/solution1.py
+++ 1-10/solution1.py
@@ -9,3 +9,39 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 https://projecteuler.net/problem=1
 
 """
+
+input = [x for x in range(0, 10)]
+test = [x for x in range(0, 1000)]
+
+
+def multiple3or5(input):
+    if not input:
+        return 0
+    else:
+        agg = 0
+        for digit in input:
+            print("digit {0}".format(digit))
+            if digit % 3 == 0 or digit % 5 == 0:
+                agg += digit
+                print("agg: {0}".format(agg))
+            else:
+                continue
+    return agg
+
+
+def sum_of_sequence(n):
+    """
+        Using N(N+1)/ 2
+    """
+    if not input:
+        return 0
+    else:
+        return (n*(n+1))/2
+
+
+print(multiple3or5(input))
+print(multiple3or5(test))
+
+print("Using Arithmetic")
+print(int(3*sum_of_sequence(999//3) + 5*sum_of_sequence(999//5) -
+      15*sum_of_sequence(999//15)))
